08e2761e0ee82b2071e02f0cc651ea22a914192f
Felix Schönbrodt <felix@nicebread.de>
2019-07-25 15:18:05 +0200

    Add /doc to Git

---
+++ package/doc/BFDA_manual.R
@@ -0,0 +1,6 @@
+## ----setup, include=FALSE------------------------------------------------
+	library(tint)
+	knitr::opts_chunk$set(cache=TRUE, warnings = FALSE, messages=FALSE)
+	# load all functions
+	devtools::load_all()
+
